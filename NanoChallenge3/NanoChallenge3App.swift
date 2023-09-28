0df266814907766d63c9d06041cc2dde641f7a25
Rafael Teivfik MacOS <rafael.teivfik2@gmail.com>
2023-09-28 14:02:42 -0300

    Initial Commit

---
+++ NanoChallenge3/NanoChallenge3App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge3App.swift
+//  NanoChallenge3
+//
+//  Created by Rafa (Ruffles) on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
